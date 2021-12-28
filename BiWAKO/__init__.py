65c62c83595c2e63e40de7750fcd76b6f769295c
NMZ0429 <gen0429@icloud.com>
2021-12-28 19:18:56 +0900

    add segmentation

--- BiWAKO/__init__.py
+++ BiWAKO/__init__.py
@@ -1,7 +1,8 @@
 __version__ = "0.0.1"
-__all__ = ["MiDASInference", "U2NetInference", "RealESRGANInference"]
+__all__ = ["MiDASInference", "U2NetInference", "RealESRGANInference", "YOLO"]
 
 from .mono_depth import MiDASInference
 from .segmentation import U2NetInference
 from .super_resolution import RealESRGANInference
+from .object_detection import YOLO
 
