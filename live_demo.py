b149aa4701babc517fb07db40899f71ee4e7e09c
NMZ0429 <gen0429@icloud.com>
2022-01-14 04:08:55 +0900

    add live prediction demo

--- live_demo.py
+++ live_demo.py
@@ -10,14 +10,14 @@ from BiWAKO import YOLO, HumanParsing, MiDAS, U2Net, YuNet, ResNet
 # model = YOLO("yolo_nano")
 # model = U2Net("mobile")
 # model = HumanParsing("human_attribute")
-# model = YuNet()
-model = ResNet("resnet18v2")
+model = YuNet()
+# model = ResNet("resnet18v2")
 
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow("frame", model.render(model.predict(frame), frame))
+    cv2.imshow("Live Prediction", model.render(model.predict(frame), frame))
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
